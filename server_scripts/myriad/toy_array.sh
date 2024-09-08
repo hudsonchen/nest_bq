c5ee8632f9da517ca347a794e52d0ab105c2b19b
hudsonchen <hudsonchen33@outlook.com>
2024-09-08 16:18:32 +0100

    server_script

--- server_scripts/myriad/toy_array.sh
+++ server_scripts/myriad/toy_array.sh
@@ -4,4 +4,4 @@ jobs_in_parallel=$(wc -l < "$1")
 echo $jobs_in_parallel
 echo $1
 
-qsub -t 1-${jobs_in_parallel} /home/ucabzc9/Scratch/nested_bq/myriad_scripts/toy_base.sh "$1"
+qsub -t 1-${jobs_in_parallel} /home/ucabzc9/Scratch/nested_bq/server_scripts/myriad/toy_base.sh "$1"
