14bacdd381a713a4f622f6ae97285e94727adce3
hudsonchen <hudsonchen33@outlook.com>
2024-10-25 17:51:26 +0100

    minor

--- server_scripts/local/toy.sh
+++
@@ -1,4 +0,0 @@
-python simulated.py --seed 0 --kernel_x matern --kernel_theta matern
-python simulated.py --seed 0 --kernel_x rbf --kernel_theta matern
-python simulated.py --seed 0 --kernel_x matern --kernel_theta rbf
-python simulated.py --seed 0 --kernel_x rbf --kernel_theta rbf
